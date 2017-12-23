ed058e68cd35ff10a1b44882b17988d05fa2dec4
wbb1975 <wbb1975@sina.com>
2017-12-23 08:20:19 +0800

    update skiplist test file

--- skiplist_test.cpp
+++ skiplist_test.cpp
@@ -25,15 +25,37 @@ int main()
 	}
 	cout << endl;
 
-	int val = 70;
-	int *found = skipList.find(val);
-	if (found == nullptr)
+	int val1 = 70;
+	int *found1 = skipList.find(val1);
+	if (found1 == nullptr)
 	{
-	    cout << "not found " << val << endl;
+	    cout << "not found " << val1 << endl;
 	}
 	else
 	{
-        cout << "found " << *found <<endl;
+        cout << "find " << val1 << " return " << *found1 <<endl;
+	}
+
+	int val2 = 10;
+	int *found2 = skipList.find(val2);
+	if (found2 == nullptr)
+	{
+	    cout << "not found " << val2 << endl;
+	}
+	else
+	{
+        cout << "find " << val2 << " return " << *found2 <<endl;
+	}
+
+	int val3 = 80;
+	int *found3 = skipList.find(val3);
+	if (found3 == nullptr)
+	{
+	    cout << "not found " << val3 << endl;
+	}
+	else
+	{
+        cout << "find " << val3 << " return " << *found3 <<endl;
 	}
 
     skipList.remove(6);
